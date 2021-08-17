67066bd7f1f78940d9d49c0930572211d4232761
mahesh <madu140@gmail.com>
2021-08-17 12:01:38 +0530

    validation

--- app.py
+++ app.py
@@ -10,22 +10,31 @@ from keras.applications.vgg16 import preprocess_input
 app = Flask(__name__)
 model = keras.models.load_model('n_model.h5')
 
+app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == "POST":
         # validate images
         imagefile = request.files['image']
-        image_path = "./images/" + imagefile.filename
-        imagefile.save(image_path)
-        image = load_img(image_path, target_size=(224,224))
-        image = img_to_array(image)
-        image = image.reshape(1,image.shape[0],image.shape[1],image.shape[2])
-        image = preprocess_input(image)
-        prediction = model.predict(image)
-        # convert image into np array
-        # and return prediction
-        # convert image in to numpy array
-    return "hello world"
+        nameOfFile = secure_filename(uploaded_file.filename)
+        if nameOfFile != '':
+            file_ext = os.path.splitext(nameOfFile)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            image_path = "./images/" + imagefile.filename
+            imagefile.save(image_path)
+            image = load_img(image_path, target_size=(224,224))
+            image = img_to_array(image)
+            image = image.reshape(1,image.shape[0],image.shape[1],image.shape[2])
+            image = preprocess_input(image)
+            prediction = model.predict(image)
+            # convert image into np array
+            # and return prediction
+            # convert image in to numpy array
+    return prediction
 
 if __name__ == "__main__":
     app.run(port=3000,debug=True)
