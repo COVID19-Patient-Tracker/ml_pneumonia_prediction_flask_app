ab00856a2e822fc873b90b115b839057050bbce2
mahesh <madu140@gmail.com>
2021-09-04 17:17:49 +0530

    retrun type fixed to string

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def predict():
             # convert image into np array
             # and return prediction
             # convert image in to numpy array
-    return prediction
+    return str(prediction)
 
 if __name__ == "__main__":
     app.run(port=3000,debug=True)
