ce1e3ad2593647d1e88b3e4872a9d8cc87335852
mahesh <madu140@gmail.com>
2021-09-24 14:47:50 +0530

    cors fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 
 
-from flask import Flask,request,cross_origin
+from flask import Flask,request, make_response
 from tensorflow import keras
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
@@ -18,8 +18,29 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif','.jpeg']
 
+from flask import 
+
+flask_app = Flask(__name__)
+
+@flask_app.route("/api/orders", methods=["POST", "OPTIONS"])
+def api_create_order():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 @app.route("/predict", methods=['POST'])
-@cross_origin()
 def predict():
     if request.method == "POST":
         # validate images
